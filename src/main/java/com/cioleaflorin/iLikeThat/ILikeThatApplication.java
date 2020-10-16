70f14e683167c7e29f4ec325c27abfee6e0ac15f
Florin <ciolea.florin.99@gmail.com>
2020-10-17 01:55:41 +0300

    Initial commit

---
+++ src/main/java/com/cioleaflorin/iLikeThat/ILikeThatApplication.java
@@ -0,0 +1,13 @@
+package com.cioleaflorin.iLikeThat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILikeThatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILikeThatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cioleaflorin/iLikeThat/ILikeThatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cioleaflorin.iLikeThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILikeThatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
